228ed5e8f68eae13ec75efea07099f25790b914c
shaythegay13 <shay.bouchles@gmail.com>
2025-08-30 17:23:17 -0400

    relax build linting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // appDir is enabled by default in Next.js 13+
+  eslint: {
+    // Warning: Disable linting during builds
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
